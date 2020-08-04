856a071d3d70e8e0e530b0cdc591bc5a09da0a43
fycoder <gemma.mobu@icloud.com>
2020-08-04 14:52:56 +0800

    message

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="(item,index) in 100" :key="index">
+        {{item}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
